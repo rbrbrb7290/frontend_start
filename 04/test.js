c87e54055ae19f05488f27c29d3ecfdab97d67c6
rbrbrb7290 <rbrbrb7290@gmail.com>
2018-09-28 05:55:55 +0900

    색상 되돌리기 className활용 실패ㅠㅠ

--- 04/test.js
+++ 04/test.js
@@ -30,48 +30,38 @@ for(var i = 0; i < 16; i++){
 wrap.innerHTML = str;
 //HTML문서의 wrap요소에 접근해 str을 넣는 과정 
 
-var boards = document.querySelectorAll('.wrap > div');
+var boards = document.querySelectorAll('.wrap > div'); //선택자 모두를 불러옴
 var selectedDom = null;
 
+//모든 선택자(boards)에 click함수를 호출하는 클릭 이벤트를 추가)
 for(var i = 0; i < 16; i++) {
     console.log(boards[i]);
     boards[i].addEventListener('click' , click)
 }
 
+// 클릭이벤트 함수
 function click(e) {
     var board = event.currentTarget;
-    console.log('Click!!');
-    board.style.backgroundColor = "red";
-   
+    console.log(className);
+    board.style.backgroundColor = "green";
+
+    if(className == 'black') 
+    { color = "black" }
+    else {color = 'white'}
 }
 
 
-// boards.addEventLister('click', selectBoard, false)
+    // if(selectedDom){
+    //     selectedDom.className = selectedDom.className.replace(' select', '');
+    // }
+    // board.className += ' select';
+    // selectedDom = board; //선택된 보드를 Dom에 저장
 
-// function selectBoard(event) {
-//     alert("click!");
-// }
-  
-//     console.log('selected');
 
-//     var board = event.currentTarge;
-//     console.log(board);
-//     board.style.backgroundColor = 'yellow';
-// }
 
-//     for(var i =0; i < 16; i++) {
-//         console.log(boards[i]);
-//         boards[i].addEventListener('click', selectBoard);
-//     }
+   
    
 
-//     if(selectedDom){
-//         selectDom.className = selectedDom.className.replace(' select', '');
-//     }
-
-//     board.className += ' select';
-
-//     selectedDom = board; //선택된 보드를 Dom에 저장
-
+    
 
 
