9ecf27f6349020acbd01ec3ded7a6d99b0101b6e
rbrbrb7290 <rbrbrb7290@gmail.com>
2018-10-12 20:57:35 +0900

    더보기완성

--- 05/boon/app1.js
+++ 05/boon/app1.js
@@ -1,9 +1,8 @@
 console.log('1boon');
+var count = 10;
+var url = `https://1boon.kakao.com/ch/enter.json?page=1&pagesize=${count}`;
 
-var url = 'https://1boon.kakao.com/ch/enter.json?page=1&pagesize=10';
-
-
-getUrlData(url, function(json){
+function print(json){
   console.log( json );
 
   var str = '';
@@ -11,12 +10,15 @@ getUrlData(url, function(json){
   for(var i=0; i < json.data.length; i++){
 
     var title = json.data[i].title;
-    var path =json.data[i].path; 
-    str += '<a href="https://1boon.kakao.com/'+path+'">' + title + '</a><br>';
+    var path =json.data[i].path; //주소값
+    // str += '<a href="https://1boon.kakao.com/'+path+'">' + title + '</a><br>';
+    str += `<a href="https://1boon.kakao.com/${path}">${title}</a><br>`
+    //여러개 문자들을 쓸필요없이 ` `를 써서 간단하게 쓸수있음
   }
 
   document.getElementById('wrap').innerHTML = str;
-})
+}
+getUrlData(url, print)
 
 
 function getUrlData(url, callback) {
@@ -28,8 +30,17 @@ function getUrlData(url, callback) {
       });
     })
     .catch(function(err) {
-      console.log('Fetch Error :-S', err);
+      console.log('Fetch Error :-S', err);//오류코드출력해줌 ex 404
     });
 }
-// var button = document.querySelectorAll('more');
-// button.addEventListener('click' , getUrlData)
+ var button = document.getElementById('more');
+ button.addEventListener('click' , function(){
+ 
+  count += 3;
+  url = `https://1boon.kakao.com/ch/enter.json?page=1&pagesize=${count}`;
+  console.log(count)//count확인
+  
+
+  getUrlData(url, print)
+
+ })
