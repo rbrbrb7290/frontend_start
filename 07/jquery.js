0411c0437c3fd2b9cce53fce240aa68bdd99fa9b
rbrbrb7290 <rbrbrb7290@gmail.com>
2018-10-12 16:49:16 +0900

    event

---
+++ 07/jquery.js
@@ -0,0 +1,30 @@
+//document.querySelector('h1').innerHTML = 'hi';
+
+// $('h1').html('hi');
+// $('h1').fadeOut().delay(1000).fadeIn();
+
+// var divs = document.querySelectorAll('div');
+// for(var i =0; i < divs.length; i++)
+// {
+//     divs[i].style.backgroundColor = 'red';
+// }
+
+
+
+var $div = $('div'); 
+$div
+.css('background-color' , 'yellow')
+.css('border', '1px solid red');
+
+var $btn = $('#btn');
+var $txt = $('#txt');
+var $debug = $('#debug');
+$('#btn').click(function(event){
+    console.log('click');
+    if($txt.val() ==''){
+        alert('값을 넣어주세요')
+        return;
+    }
+
+    $debug.html($txt.val());
+});
