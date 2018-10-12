d5c51beadd7537aecf988f33dde25759b9d29013
rbrbrb7290 <rbrbrb7290@gmail.com>
2018-10-12 17:31:30 +0900

    요소들 사라지게했다가 다시 나타나게하기

--- 07/jquery.js
+++ 07/jquery.js
@@ -11,14 +11,22 @@
 
 
 
-var $div = $('div'); 
-$div
-.css('background-color' , 'yellow')
-.css('border', '1px solid red');
+// var $div = $('div'); 
+// $div
+// .css('background-color' , 'yellow')
+// .css('border', '1px solid red');
 
 var $btn = $('#btn');
 var $txt = $('#txt');
 var $debug = $('#debug');
+
+
+$('.wrap').on('click' , 'div' , function(event){
+    console.log(event.target);
+    $(event.target).fadeOut();
+
+})
+
 $('#btn').click(function(event){
     console.log('click');
     // if($txt.val() ==''){
@@ -27,5 +35,9 @@ $('#btn').click(function(event){
     // }
 
     // $debug.html($txt.val());
-    $('h1').hide('slow'); //fadeout()
+    // $('h1').hide('slow'); //fadeout()
+    // $('h1').show('slow'); //fadeout()
+
+    $('.wrap div').show('slow');
+    
 });
